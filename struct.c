184b1e9c18453785c28e881e8a7d7245d5fdf3bb
chris adams <cadams00@gmail.com>
2024-09-04 02:54:48 -0500

    added struct

--- struct.c
+++ struct.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define MAX_EMPLOYEES 1000
 
@@ -14,9 +15,22 @@ struct employee_t
 
 int main(int argc, char const *argv[])
 {
-	struct employee_t Fred;
-	Fred.income = 100.00;
-	Fred.ismanager = true;
-
+	struct employee_t employees[MAX_EMPLOYEES];
+	int i = 0;
+	for (int i = 0; i < MAX_EMPLOYEES; ++i) {
+		employees[i].id = i;
+		strcpy(employees[i].firstname, "empty");
+		strcpy(employees[i].lastname, "empty");
+		employees[i].income = 0;
+		employees[i].ismanager = false;
+	}
+	for (int i = 0; i < MAX_EMPLOYEES; ++i)
+	{
+		printf("The Employee's id is %d\n", employees[i].id);
+		printf("The Employee's Full Name is %s %s\n", employees[i].firstname, employees[i].lastname);
+		printf("The Employee's income is %f\n", employees[i].income);
+		printf("The Employee is a manager %b\n", employees[i].ismanager);
+		printf("\n");
+	}
 	return 0;
 }
